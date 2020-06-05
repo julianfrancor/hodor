927fd14da1d94986e36a9db5f910e59be9ce708a
julianfrancor <julianfrancor@gmail.com>
2020-06-05 01:52:02 +0000

    task 2

--- level_2/level_2.py
+++ level_2/level_2.py
@@ -9,7 +9,6 @@ host = "http://158.69.76.135/level2.php"
 for i in range(1024):
     response = requests.get('http://158.69.76.135/level2.php')
     header = {
-        "Content-Type": "application/x-www-form-urlencoded",
         "Referer": "http://158.69.76.135/level2.php",
         "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0WOW64 rv: 44.0) Gecko/20100101 Firefox/44.0",
